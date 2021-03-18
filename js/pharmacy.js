eea7bcdaf7e7793b05e02d03af5873a529b0136a
Frank Farm <frank.farm@ucsf.edu>
2021-03-18 08:15:14 -0700

    lots of things including heading spacing adjustments, abandoned (commented out) experiments with automatic multicolumns, multicolumns, layout builder adjustments, article, in the news, and more

--- js/pharmacy.js
+++ js/pharmacy.js
@@ -1,3 +1,23 @@
+// start - apply automatic columns when there is at least 600 characters
+// frankfarm - this was an experiment to see if i could automatically set multicolumns based on the length of the content. it does not work because not everything is large blocks of headings and text. tabled for now.
+/*
+var items = document.querySelectorAll('.field');
+for (var i = 0; i < items.length; i++) {
+  var item_length = items[i].textContent.replace(/\s/g,'').length;
+  if (
+    ( ! items[i].classList.contains('laurel-no-columns') )
+    && ( item_length >= 600 )
+  ){
+    items[i].classList.add('columns','auto','length-' + item_length);
+  }
+}
+*/
+// end - apply automatic columns when there is at least 600 characters
+
+// jQuery(document).ready(function($) {
+  // put jquery stuff here
+// })(jQuery);
+
 // start - https://every-layout.dev/layouts/grid/
 // frankfarm: i'm not sure we need this
 /*
